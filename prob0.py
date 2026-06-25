0258bf0a6dbc1fb7e304dc92891a2fa81cd41324
Adhrit15 <gandheadhrit1503@gmail.com>
2026-06-25 23:09:54 +0530

    solution

---
+++ prob0.py
@@ -0,0 +1,11 @@
+#Problem Zero 
+
+#Create a list of five floats 
+#Create  a list of five string 
+
+#print both of them 
+
+f_list = [1.5, 2.75, 3.14, 4.2, 5.99]
+s_list = ["apple", "banana", "cherry", "date", "elderberry"]
+print("Float List:", f_list)
+print("String List:", s_list)
